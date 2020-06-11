0b2c18069ff39fcbe9f74aaf8723e50eb732aa24
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-06-11 13:12:22 +0700

    set project but still unfinished yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Alert,Button } from 'reactstrap';
+import {
+  GoogleMap,withScriptjs,withGoogleMap
+} from 'react-google-maps';
+import mapStyles from './mapStyles'
 import './App.css';
 
-function App() {
+const center={
+  lat:54.525963,
+  lng:15.255119
+}
+
+const options={
+  styles:mapStyles,
+  disabledDefaultUI:false,
+  zoomControl:false
+}
+
+function Map(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GoogleMap defaultZoom={5} defaultCenter={center} defaultOptions={options} onClick={(event)=>{
+      console.log('Map kepencet',{lat:event.latLng.lat(),lng:event.latLng.lng()});
+    }} />
+  )
+}
+
+const WrappedMap=withScriptjs(withGoogleMap(Map));
+
+function App() {
+  console.log(process.env.REACT_APP_GOOGLE_MAP_API_KEY,'api key')
+
+  return (
+    <div className="App container"> 
+      <Alert color="info">
+        5 cities placed
+      </Alert>
+      <Alert color="info">
+       361 kilometers left
+      </Alert>
+      <p>
+        Select the location of "Amsterdam"
+      </p>
+      <div id="map">
+        <WrappedMap  googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLE_MAP_API_KEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+      <div className="d-flex flex-row-reverse mt-2">
+        <Button color="info">place</Button>
+      </div>
     </div>
   );
 }
---
+++ src/mapStyles.js
@@ -0,0 +1,51 @@
+export default[
+    {
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ]
